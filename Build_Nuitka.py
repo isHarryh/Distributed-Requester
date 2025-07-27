30c5b4bb69fc868e036062fad5ec8a1d8d7b35c2
Harry Huang <harryhuang2652@qq.com>
2025-07-27 21:03:28 +0800

    feat: optimize package method

--- Build_Nuitka.py
+++ Build_Nuitka.py
@@ -4,69 +4,65 @@
 import os
 import sys
 import base64
+import toml
 from pathlib import Path
 
-root_path = Path(sys.argv[0]).resolve().parent
 
-os.system(
-    "powershell -Command python -m nuitka --standalone --onefile --mingw64"
-    " --onefile-tempdir-spec='{TEMP}\\DIST_REQUESTER'"
-    " --file-version=0.1 --product-version=0.1"
-    " --assume-yes-for-downloads --output-filename=Core"
-    " --remove-output Main.py"
-)
+if __name__ == "__main__":
+    root_path = Path(sys.argv[0]).resolve().parent
 
-with (root_path / "Core.exe").open("rb") as f:
-    data = f.read()
-encoded = base64.b64encode(data).decode("utf-8")
+    config = toml.load("pyproject.toml")
 
-with (root_path / "do.py").open("w") as f:
-    f.write(
-        f"""
-import sys
-import subprocess
-import base64
-from pathlib import Path
+    version = config["project"]["version"]
 
-root_path = Path(sys.argv[0]).resolve().parent
-
-core_exe_data = "{encoded}"
-
-if not (root_path / "Core.exe").exists():
-    print("creating Core.exe from base64 data...")
-    (root_path / "Core.exe").write_bytes(base64.b64decode(core_exe_data))
-
-if not (root_path / "config.json").exists():
-    print("creating config.json...")
-    (root_path / "config.json").write_text(
-        r'''
-{{
-    "version": "0.1",
-    "tasks": [],
-    "client": {{
-        "server_url": "http:\\/\\/boom.harryh.cn",
-        "report": {{
-            "live_report_interval": 10
-        }}
-    }}
-}}
-'''
+    os.system(
+        "powershell -Command python -m nuitka --standalone --onefile --mingw64"
+        " --onefile-tempdir-spec='{TEMP}\\DIST_REQUESTER'"
+        f" --file-version={version} --product-version={version}"
+        " --assume-yes-for-downloads --output-filename=Core"
+        " --remove-output Main.py"
     )
 
-subprocess.run(
-    ["Core.exe", "--client", "config.json"],
-)
-"""
+    with (root_path / "Core.exe").open("rb") as f:
+        binary_data = f.read()
+    encoded_binary = base64.b64encode(binary_data).decode("utf-8")
+
+    with (root_path / "config.json").open("rb") as f:
+        default_config = f.read()
+    encoded_base64 = base64.b64encode(default_config).decode("utf-8")
+
+    with (root_path / "do.py").open("w") as f:
+        f.write(
+            f"""
+    import sys
+    import subprocess
+    import base64
+    from pathlib import Path
+
+    root_path = Path(sys.argv[0]).resolve().parent
+
+    core_exe_data = "{encoded_binary}"
+    config_data = "{encoded_base64}"
+
+    if not (root_path / "Core.exe").exists():
+        print("creating Core.exe from base64 data...")
+        (root_path / "Core.exe").write_bytes(base64.b64decode(core_exe_data))
+
+    if not (root_path / "config.json").exists():
+        print("creating config.json...")
+        (root_path / "config.json").write_bytes(base64.b64decode(config_data))
+
+    subprocess.run(["Core.exe", "--client", "config.json"])
+    """
+        )
+
+    os.system(
+        "powershell -Command python -m nuitka --standalone --onefile --mingw64"
+        " --onefile-tempdir-spec='{TEMP}\\DIST_REQUESTER'"
+        " --file-version=0.1 --product-version=0.1"
+        " --assume-yes-for-downloads --output-filename=JustDoIt"
+        " --remove-output do.py"
     )
 
-(root_path / "Core.exe").unlink()
-
-os.system(
-    "powershell -Command python -m nuitka --standalone --onefile --mingw64"
-    " --onefile-tempdir-spec='{TEMP}\\DIST_REQUESTER'"
-    " --file-version=0.1 --product-version=0.1"
-    " --assume-yes-for-downloads --output-filename=JustDoIt"
-    " --remove-output do.py"
-)
-
-(root_path / "do.py").unlink()
+    (root_path / "Core.exe").unlink()
+    (root_path / "do.py").unlink()
